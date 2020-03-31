04417fc336117905e22320d86b080fe113c03aff
Janice Muia <janiceink001@gmail.com>
2020-03-31 20:42:15 +0300

    align items center

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {Text, View, Stylesheet} from 'react-native';
 
 const styles = StyleSheet.create({
+  center:{
+    alignItems:'center'
+  }
 
 
 
 
-  
+
 })
  
 
